7c94646ad142c4cb150d80b2f9f3887307e670bb
PHẠM Tuấn Anh <cs.PhamTuanAnh@gmail.com>
2009-11-05 01:26:50 +0700

    remove empty line between php codes, so header(redirect) works, remove all unuse html code in user/test.php

--- user/test.php
+++ user/test.php
@@ -12,7 +12,6 @@ include_once "../lib/Database.php";
 
 	$student = $_SESSION['student'];
 ?>
-
 <?php
 	$db = new Database ($db_server, $db_username, $db_password);
 	$db->selectDatabase ($db_database);
@@ -60,86 +59,3 @@ include_once "../lib/Database.php";
 
 	header ('Location: question.php');
 ?>
-
-<HTML>
-<HEAD>
-	<title>OES Admin - Test</title>
-</HEAD>
-
-<BODY>
-<div align=center>
-	<h1>Tạo đợt thi mới</h1>
-
-	<form action=exam_list.php method=POST>
-		<table>
-			<tr><td><label for=name>Tên</label>
-				<td><input id=name name=name>
-
-			<tr><td><label for=class>Lớp</label>
-				<td><select id=class name=class>
-						<option value=0>Tạo mới</option>
-						<?php
-							$arr = $db->getClassList();
-							foreach ($arr as $class)
-								echo "<option value=$class[1]>$class[0]</option>";
-						?>
-					</select>
-					<input id=newclass name=newclass>
-
-			<tr><td><label for=subject>Môn</label>
-				<td><select id=subject name=subject>
-						<option value=0>Tạo mới</option>
-						<?php
-							$arr = $db->getSubjectList();
-							foreach ($arr as $subject)
-								echo "<option value=$subject[1]>$subject[0]</option>";
-						?>
-					</select>
-					<input id=newsubject name=newsubject>
-
-			<tr><td><label for=time>Lần</label>
-				<td><input id=time name=time value=1>
-
-			<tr><td><label for=teacher>Giáo viên</label>
-				<td><select id=teacher name=teacher>
-						<option value=0>Tạo mới</option>
-						<?php
-							$arr = $db->getTeacherList();
-							foreach ($arr as $teacher)
-							{
-								$tname = $teacher[0] . ' ' . $teacher[1];
-								echo "<option value=$teacher[2]>$tname</option>";
-							}
-						?>
-					</select>
-					<input id=newteacher name=newteacher>
-
-			<tr><td><label for=duration>Thời gian</label>
-				<td><input id=duration name=duration value=90> phút
-
-			<tr><td><label for=sched_date>Ngày  thi</label>
-				<td><script language="JavaScript">
-						var today = f_tcalGenerDate (new Date());
-						document.writeln ("<input id=sched_date name=sched_date value=" + today + ">");
-						new tcal ({'controlname':'sched_date'});
-					</script>
-
-			<tr><td><label for=sched_hour>Giờ thi</label>
-				<td><input id=sched_hour name=sched_hour value=15:00> (HH:mm)
-
-			<tr><td><label for=noq>Số câu hỏi</label>
-				<td><input id=noq name=noq value=30>
-
-			<tr><td><label for=max_noc>Số lựa chọn tối đa</label>
-				<td><input id=max_noc name=max_noc value=4>
-
-			<tr align=center>
-				<td colspan=2>
-					<input type=reset value=Huỷ>
-					<input type=submit name=submit value='Hoàn thành'>
-		</table>
-	</form>
-
-</div>
-</BODY>
-</HTML>
