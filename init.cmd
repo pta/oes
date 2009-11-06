f91cf3ea485d50bf4b3ca6f94a6d3bf29fcc896c
PHẠM Tuấn Anh <cs.PhamTuanAnh@gmail.com>
2009-11-06 22:58:48 +0700

    add database initialize script for Windows deployment

---
+++ init.cmd
@@ -0,0 +1,8 @@
+
+\xampplite\mysql\bin\mysql -u root -e"source sql/database.sql"
+
+\xampplite\mysql\bin\mysql -u pta_home -p123456 -D pta_home -e"source sql/structure.sql"
+
+\xampplite\mysql\bin\mysql -u pta_home -p123456 -D pta_home -e"source sql/data.sql"
+
+@PAUSE
