91afe4fbabd9875f7fd3b2afbb244353049262cf
PHẠM Tuấn Anh <cs.PhamTuanAnh@gmail.com>
2009-11-09 06:33:53 +0700

    support script module

--- ptajax/module.js
+++ ptajax/module.js
@@ -1,13 +1,13 @@
 
 function onIFrameLoad (iFrame, name)
 {
-	var content = iFrame.contentWindow.document.body.innerHTML
+	var content = iFrame.contentWindow.document.body.innerHTML;
 	document.getElementById (name).innerHTML = content;
 }
 
 function insertModule (name, src)
 {
-	document.writeln ("<div id='" + name + "'>Loading...</div>")
+	document.writeln ("<div id='" + name + "'>Loading...</div>");
 	document.writeln ("<iframe frameborder=0 id='iFrame_" + name
 			+ "' onload='onIFrameLoad(this,\"" + name
 			+ "\")'></iframe>");
@@ -18,8 +18,33 @@ function insertModule (name, src)
 function loadModule (name, src)
 {
 	if (src == null)
-		src = "modules.php?id=" + name
+		src = "modules.php?id=" + name;
 
-	var iFrame = document.getElementById ("iFrame_" + name)
-	iFrame.src = src
+	var iFrame = document.getElementById ("iFrame_" + name);
+	iFrame.src = src;
+}
+
+function insertScriptModule (name, src)
+{
+	document.writeln ("<div id='" + name + "'>Loading...</div>");
+	document.writeln ("<iframe frameborder=0 id='iFrame_" + name
+			+ "' onload='onIFrameScriptLoad(this,\"" + name
+			+ "\")'></iframe>");
+
+	loadModule (name, src);
+}
+
+function onIFrameScriptLoad (iFrame, name)
+{
+	var content = iFrame.contentWindow.document.body.innerHTML;
+
+	var start, end = 0;
+	while ((start = content.indexOf ('<div class="script">', end)) >= 0)
+	{
+		end = content.indexOf ('</div>', start);
+		var script = content.slice (start + 20, end);
+		eval (script);
+	}
+
+	document.getElementById (name).innerHTML = content;
 }
