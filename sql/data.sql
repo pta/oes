8fac85cf72d5552bb17e93829729dbc99f408d46
PHẠM Tuấn Anh <cs.PhamTuanAnh@gmail.com>
2009-11-09 15:21:00 +0700

    new sample data.sql

--- sql/data.sql
+++ sql/data.sql
@@ -4,13 +4,9 @@ insert into Class values (null, "CN1K7D", 7);
 insert into Class values (null, "CN1K8G", 8);
 insert into Class values (null, "CN1K6G", 6);
 
-insert into Teacher values (null, "Vân", "Thái Thị Thanh");
-insert into Teacher values (null, "Quang", "Đào Hào");
+insert into Teacher values (null, "Phèo", "Nguyễn Chí");
+insert into Teacher values (null, "Nở", "Phạm Thị");
 
 insert into Subject values (null, "Xử lý ảnh");
 insert into Subject values (null, "Trí tuệ nhân tạo");
-insert into Subject values (null, "Cấu trúc dữ liệu và giải thuật I");
-insert into Subject values (null, "Cấu trúc dữ liệu và giải thuật II");
-insert into Subject values (null, "C++");
-insert into Subject values (null, "C for Windows");
-insert into Subject values (null, "Thiết kế và lập trình Web");
+insert into Subject values (null, "Cấu trúc dữ liệu và giải thuật");
