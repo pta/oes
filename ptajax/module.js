e1d124a66ff30a7f0c56eb21774ee70fec8a4831
PHẠM Tuấn Anh <cs.PhamTuanAnh@gmail.com>
2009-11-10 00:59:45 +0700

    make forms in iframe target to corresponding divs

--- ptajax/module.js
+++ ptajax/module.js
@@ -1,14 +1,23 @@
 
 function onIFrameLoad (iFrame, name)
 {
-	var content = iFrame.contentWindow.document.body.innerHTML;
+	var ifrmDoc = iFrame.contentWindow.document;
+
+	for (var i = 0; i < ifrmDoc.forms.length; ++i)
+		ifrmDoc.forms[i].target = 'ifrm_' + name;
+
+	var content = ifrmDoc.body.innerHTML;
+
 	document.getElementById (name).innerHTML = content;
 }
 
 function insertModule (name, src)
 {
+	var id = 'ifrm_' + name;
+
 	document.writeln ("<div id='" + name + "'>Loading...</div>");
-	document.writeln ("<iframe frameborder=0 id='iFrame_" + name
+	document.writeln ("<iframe frameborder=0 id='" + id
+			+ "' name='" + id
 			+ "' onload='onIFrameLoad(this,\"" + name
 			+ "\")'></iframe>");
 
@@ -20,14 +29,17 @@ function loadModule (name, src)
 	if (src == null)
 		src = "modules.php?id=" + name;
 
-	var iFrame = document.getElementById ("iFrame_" + name);
+	var iFrame = document.getElementById ("ifrm_" + name);
 	iFrame.src = src;
 }
 
 function insertScriptModule (name, src)
 {
+	var id = 'ifrm_' + name;
+
 	document.writeln ("<div id='" + name + "'>Loading...</div>");
-	document.writeln ("<iframe frameborder=0 id='iFrame_" + name
+	document.writeln ("<iframe frameborder=0 id='" + id
+			+ "' name='" + id
 			+ "' onload='onIFrameScriptLoad(this,\"" + name
 			+ "\")'></iframe>");
 
@@ -36,7 +48,12 @@ function insertScriptModule (name, src)
 
 function onIFrameScriptLoad (iFrame, name)
 {
-	var content = iFrame.contentWindow.document.body.innerHTML;
+	var ifrmDoc = iFrame.contentWindow.document;
+
+	for (var i = 0; i < ifrmDoc.forms.length; ++i)
+		ifrmDoc.forms[i].target = 'ifrm_' + name;
+
+	var content = ifrmDoc.body.innerHTML;
 
 	var start, end = 0;
 	while ((start = content.indexOf ('<div class="script">', end)) >= 0)
