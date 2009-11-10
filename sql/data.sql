5e845a8f9ff7b08f47fe8c4dc26933e62ad5c040
PHẠM Tuấn Anh <cs.PhamTuanAnh@gmail.com>
2009-11-11 01:14:25 +0700

    add sample Exam and Student data

--- sql/data.sql
+++ sql/data.sql
@@ -14,3 +14,8 @@ insert into Teacher values (null, "Nở", "Phạm Thị");
 insert into Subject values (null, "Xử lý ảnh");
 insert into Subject values (null, "Trí tuệ nhân tạo");
 insert into Subject values (null, "Cấu trúc dữ liệu và giải thuật");
+
+insert into Exam values (null, "ABC", 1, 1, 1, 1, 60,
+	'2009-11-11 15:00:00', '2009-11-11 15:00:00', null, 26, 5, 1);
+
+insert into Student values (null, "0", "0", "0", "1945-09-02", 1);
