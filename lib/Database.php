cc2734173cf632ffe88fa70487486260a0a38238
PHẠM Tuấn Anh <cs.PhamTuanAnh@gmail.com>
2009-11-13 01:13:13 +0700

    update random exam generator

--- lib/Database.php
+++ lib/Database.php
@@ -183,24 +183,24 @@ class Database extends DBConnection
 
 	function insertRandomExam()
 	{
-		$noq = mt_rand (6, 13);
+		$noq = 13;
 
 		for ($i = 0; $i < $noq; ++$i)
 		{
 			$this->query ('insert ignore into Exam values (
 					null,
 					left(md5(rand()),6),
-					1 + floor (rand() * (select count(*) from Class)),
-					1 + floor (rand() * (select count(*) from Subject)),
-					1 + floor (rand() * 4),
-					1 + floor (rand() * (select count(*) from Teacher)),
-					(1 + floor (rand() * 4)) * 30,
+					0.5 + rand() * (select count(*) from Class),
+					0.5 + rand() * (select count(*) from Subject),
+					0.5 + rand() * 4,
+					0.5 + rand() * (select count(*) from Teacher),
+					round (0.5 + rand() * 4) * 30,
 					NOW() + INTERVAL (RAND() * 91 - 45) DAY + INTERVAL (RAND() * 172801 - 86400) SECOND,
 					if (rand()<0.6, null, NOW() - INTERVAL (RAND() * 45) DAY + INTERVAL (RAND() * 172801 - 86400) SECOND),
 					if (rand()<0.8, null, NOW() - INTERVAL (RAND() * 45) DAY + INTERVAL (RAND() * 172801 - 86400) SECOND),
-					20 + floor (rand() * 21),
-					4 + floor (rand() * 7),
-					round (rand()))');
+					(4 + floor (rand() * 5)) * 5,
+					3 + floor (rand() * 3),
+					rand())');
 		}
 
 		$this->query ('update Exam set Start_Time = Sched_Time where End_Time is not null and Start_Time is null');
