9a74be4471bf5e77199554735bba83b48dccd5ac
PHẠM Tuấn Anh <cs.PhamTuanAnh@gmail.com>
2009-11-14 12:31:55 +0700

    use plain iframe for admin/index.php instead of ajax module

--- admin/exam.php
+++ admin/exam.php
@@ -1,4 +1,7 @@
 <?php
+include_once "../config.php";
+?>
+<?php
 	session_start();
 	if (!isset ($_SESSION['user']))
 	{
@@ -10,6 +13,33 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 	<script type="text/javascript" src="../ptajax/module.js"></script>
+	<link href="../ptajax/module.css" rel="stylesheet" type="text/css">
+	<link href="exam.css" rel="stylesheet" type="text/css">
+
+	<script>
+		var statInterval;
+		var exam;
+
+		function onAutoStat()
+		{
+			loadModule ('stat', 'exam_modules.php?id=stat&exam=' + exam);
+		}
+
+		function setAutoStat (ex)
+		{
+			exam = ex;
+			clearAutoStat();
+			statInterval = setInterval ('onAutoStat()',
+					<?php echo STAT_RELAD_INTERVAL?>);
+			onAutoStat();
+		}
+
+		function clearAutoStat()
+		{
+			if (statInterval)
+				clearInterval (statInterval);
+		}
+	</script>
 </head>
 <body>
 	<script>insertModule ('list', 'exam_modules.php?id=list');</script>
--- admin/index.php
+++ admin/index.php
@@ -1,53 +1,23 @@
-<?php
-include_once "../config.php";
-?>
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 	<title>OES Admin</title>
-
 	<link href="index.css" rel="stylesheet" type="text/css">
-	<link href="../ptajax/module.css" rel="stylesheet" type="text/css">
-	<script type="text/javascript" src="../ptajax/module.js"></script>
-
-	<script>
-		var statInterval;
-		var exam;
-
-		function onAutoStat()
-		{
-			loadModule ('stat', 'exam_modules.php?id=stat&exam=' + exam);
-		}
-
-		function setAutoStat (ex)
-		{
-			exam = ex;
-			clearAutoStat();
-			statInterval = setInterval ('onAutoStat()',
-					<?php echo STAT_RELAD_INTERVAL?>);
-			onAutoStat();
-		}
-
-		function clearAutoStat()
-		{
-			if (statInterval)
-				clearInterval (statInterval);
-		}
-	</script>
 </head>
 
 <body>
-<div id=all>
-	<div id=control>
-		<a href=# onClick="loadModule('main','exam.php')">Các buổi thi</a>
-		<a href=# onClick="loadModule('main','exam_new.php')">Tạo buổi thi mới</a>
-		<br>
-		<a href=# onClick="loadModule('main','sample_db.php')">Sinh dữ liệu mẫu</a>
-		<a href=# onClick="loadModule('main','question_new.php')">Thêm câu hỏi</a>
-		<br>
-		<a href=# onClick="loadModule('main','login.php')">Thoát</a>
-	</div>
-	<script>insertModule ('main', 'exam.php')</script>
-</div>
+	<table id=all>
+		<td><div id=control>
+				<a target=main href=exam.php>Các buổi thi</a>
+				<a target=main href=exam_new.php>Tạo buổi thi mới</a>
+				<br>
+				<a target=main href=sample_db.php>Sinh dữ liệu mẫu</a>
+				<a target=main href=question_new.php>Thêm câu hỏi</a>
+				<br>
+				<a target=main href=login.php>Thoát</a>
+			</div>
+		<td width=100%>
+			<iframe id=main name=main src='exam.php'></iframe>
+	</table>
 </body>
 </html>
